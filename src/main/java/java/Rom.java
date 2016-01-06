9d7ea1cd8fd36eab93b5ee037caf45358e5e1eda
Sondre <Sondre@158.38.190.99>
2016-01-06 12:13:13 +0100

    Lagt til booleanske verdier i kobleTil i database

--- src/main/java/java/Rom.java
+++ src/main/java/java/Rom.java
@@ -15,6 +15,68 @@ public class Rom {
     int plasser;
     boolean harSmartboard;
     boolean harSkjerm;
-    boolean harProsjektør;
+    boolean harProsjektor;
     int tilgang;
+    
+    public Rom() {
+        
+    }
+
+    public String getRomnr() {
+        return romnr;
+    }
+
+    public void setRomnr(String romnr) {
+        this.romnr = romnr;
+    }
+
+    public int getEtasje() {
+        return etasje;
+    }
+
+    public void setEtasje(int etasje) {
+        this.etasje = etasje;
+    }
+
+    public int getPlasser() {
+        return plasser;
+    }
+
+    public void setPlasser(int plasser) {
+        this.plasser = plasser;
+    }
+
+    public boolean isHarSmartboard() {
+        return harSmartboard;
+    }
+
+    public void setHarSmartboard(boolean harSmartboard) {
+        this.harSmartboard = harSmartboard;
+    }
+
+    public boolean isHarSkjerm() {
+        return harSkjerm;
+    }
+
+    public void setHarSkjerm(boolean harSkjerm) {
+        this.harSkjerm = harSkjerm;
+    }
+
+    public boolean isHarProsjektor() {
+        return harProsjektor;
+    }
+
+    public void setHarProsjektor(boolean harProsjektør) {
+        this.harProsjektor = harProsjektor;
+    }
+
+    public int getTilgang() {
+        return tilgang;
+    }
+
+    public void setTilgang(int tilgang) {
+        this.tilgang = tilgang;
+    }
 }
+
+
