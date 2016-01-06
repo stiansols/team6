76ec07d1749839c5aa2842333c4b153ceede6afd
Svein <Svein@158.38.211.219>
2016-01-06 14:27:40 +0100

    Hjelpemetode

--- src/main/java/java/Administrator.java
+++ src/main/java/java/Administrator.java
@@ -5,10 +5,60 @@
  */
 package java;
 
+import database.DbConnection;
+
 /**
  *
  * @author Archimedes
  */
 public class Administrator extends Bruker {
+    
+    public Administrator(){
+
+    }
+
+    public String getBrukernavn() {
+        return brukernavn;
+    }
+
+    public void setBrukernavn(String brukernavn) {
+        this.brukernavn = brukernavn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getPassord() {
+        return passord;
+    }
+
+    public void setPassord(String passord) {
+        this.passord = passord;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    public void RegBruker(String brukernavn, String navn,String passord,String email,int type)throws Exception{
+        DbConnection conn = new DbConnection();
+        conn.executeQuery("Insert into bruker (brukernavn,navn,passord,mail) values('"+brukernavn+"'"+",'"+navn+"'"+",'"+passord+"'"+",'"+email+"'"+","+type+""+")");
+    }
+    
+    public void slettBruker(String brukernavn)throws Exception{
+        DbConnection conn = new DbConnection();
+        conn.executeQuery("delete from bruker where brukernavn = '"+brukernavn+"'");
+    }
+    
+    
     
 }
