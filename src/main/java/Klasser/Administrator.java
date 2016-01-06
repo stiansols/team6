9f18d29c5c4e2c085a2944fc6d399c5699430423
Sondre <Sondre@158.38.190.99>
2016-01-06 16:33:10 +0100

    Administrator

--- src/main/java/Klasser/Administrator.java
+++ src/main/java/Klasser/Administrator.java
@@ -52,12 +52,12 @@ public class Administrator extends Bruker {
     
     public void RegBruker(String brukernavn,int brukertype, String navn,String passord,String email,int type)throws Exception{
         DbConnection conn = new DbConnection();
-        conn.executeQuery("Insert into bruker (brukernavn,brukertype,navn,passord,mail) values('"+brukernavn+"'"+",'"+brukertype+"','"+navn+"'"+",'"+passord+"'"+",'"+email+"'"+")");  //","+type+""+")");
+        conn.executeUpdate("Insert into bruker (brukernavn,brukertype,navn,passord,mail) values('"+brukernavn+"'"+",'"+brukertype+"','"+navn+"'"+",'"+passord+"'"+",'"+email+"'"+")");  //","+type+""+")");
     }
     
     public void slettBruker(String brukernavn)throws Exception{
         DbConnection conn = new DbConnection();
-        conn.executeQuery("delete from bruker where brukernavn = '"+brukernavn+"'");
+        conn.executeUpdate("delete from bruker where brukernavn = '"+brukernavn+"'");
     }
     
    
