22d9f5968f6d4d6f94b8ef55117a38a85835390f
Sondre <Sondre@158.38.190.99>
2016-01-06 14:28:18 +0100

    Lagt til booleanske verdier i kobleTil i database

---
+++ src/main/java/java/Etasje.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sondre
+ */
+
+public class Etasje {
+    int etasjenr; 
+    ArrayList<Rom> romliste = new ArrayList(); 
+    
+    public Etasje(){
+        
+    }
+
+    public ArrayList<Rom> getRomliste() {
+        return romliste;
+    }
+
+    public void setRomliste(ArrayList<Rom> romliste) {
+        this.romliste = romliste;
+    }
+
+    public int getEtasjenr() {
+        return etasjenr;
+    }
+
+    public void setEtasjenr(int etasjenr) {
+        this.etasjenr = etasjenr;
+    }
+    
+}
