b443f69b24554505575dfda04d2a04d255d71b29
Stian <Stian@DESKTOP-M6AVA4R>
2016-01-06 08:31:01 +0100

    databaseklassen er lagt til

---
+++ src/main/java/database/DbConnection.java
@@ -0,0 +1,39 @@
+package database;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
+
+public class DbConnection {
+    public static void main(String[] args) throws Exception {
+ 
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+        DataSource dataSource = (DataSource) appContext.getBean("dataSource");
+ 
+        Connection connection = dataSource.getConnection();
+        System.out.println("Database with data source connected!");
+        String result = query(connection);
+        System.out.println(result);
+    }
+ 
+        private static String query(Connection connection) throws SQLException{
+ 
+        String query="";
+         Statement statement = connection.createStatement();
+ 
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM table1");
+ 
+        while(resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String nimi = resultSet.getString("name");  
+ 
+            query = query + id + "\t" + nimi + "\n";
+        }
+        return query;
+    }
+ 
+}
