b977df94940b97a6ea1063c47eb2db3ed4e5a7d2
Seva <Seva@192.168.1.3>
2016-01-24 22:43:45 +0100

    junit

--- src/test/java/database/DbConnectionTest.java
+++ src/test/java/database/DbConnectionTest.java
@@ -83,53 +83,6 @@ public class DbConnectionTest {
     }
 
     /**
-     * Test of hentAlleBrukere method, of class DbConnection.
-     */
-    @Test
-    public void testHentAlleBrukere() throws Exception {
-        System.out.println("hentAlleBrukere");
-        String expResult = "AntonioJo, 0, Antoniosss, da39a3ee5e6b4b0d3255bfef95601890afd80709, antoio1@mail.com";
-        ArrayList<Bruker> res = instance.hentAlleBrukere();
-        Bruker result = res.get(0);
-        assertEquals(expResult, result.getBrukernavn() + ", " + result.getBrukertype() + ", " + result.getNavn() + ", " + result.getPassord() + ", " + result.getMail());
-        
-    }
-
-    /**
-     * Test of hentAlleBookinger method, of class DbConnection.
-     */
-    @Test
-    public void testHentAlleBookinger() throws Exception {
-        System.out.println("hentAlleBookinger");
-        String expResult = "[Booking: 173 \n" +
-" Romnr: 102 \n" +
-" Fra: 30-01-2016-06-00 \n" +
-" Til: 30-01-2016-07-00 \n" +
-" Sjekket inn: true, Booking: 303 \n" +
-" Romnr: 102 \n" +
-" Fra: 25-02-2016-14-00 \n" +
-" Til: 25-02-2016-15-00 \n" +
-" Sjekket inn: false, Booking: 304 \n" +
-" Romnr: 107 \n" +
-" Fra: 28-01-2016-06-00 \n" +
-" Til: 28-01-2016-09-00 \n" +
-" Sjekket inn: false, Booking: 335 \n" +
-" Romnr: 107 \n" +
-" Fra: 25-01-2016-09-00 \n" +
-" Til: 25-01-2016-15-00 \n" +
-" Sjekket inn: false, Booking: 363 \n" +
-" Romnr: 108 \n" +
-" Fra: 31-01-2016-11-00 \n" +
-" Til: 31-01-2016-15-00 \n" +
-" Sjekket inn: false, Booking: 368 \n" +
-" Romnr: 108 \n" +
-" Fra: 25-01-2016-09-00 \n" +
-" Til: 25-01-2016-15-00 \n";
-        ArrayList result = instance.hentAlleBookinger();
-        assertEquals(expResult, result.toString());
-    }
-
-    /**
      * Test of hentBruker method, of class DbConnection.
      */
     @Test
@@ -139,57 +92,6 @@ public class DbConnectionTest {
         String expResult = "AntonioJo, 0, Antoniossssss, da39a3ee5e6b4b0d3255bfef95601890afd80709, antoio1@mail.com";
         Bruker result = instance.hentBruker(brukernavn);
         assertEquals(expResult, result.getBrukernavn() + ", " + result.getBrukertype() + ", " + result.getNavn() + ", " + result.getPassord() + ", " + result.getMail());
-    }
-
-    /**
-     * Test of hentAlleFag method, of class DbConnection.
-     */
-    @Test
-    public void testHentAlleFag() throws Exception {
-        System.out.println("hentAlleFag");
-        String expResult = "[fagkode, fagnavn][IT-01, IT][TDAT-1000 , Datasys][TDAT-2001 , Algoritmer og datastrukturer][TDAT-2002 , Systemutvikling][TDAT-9001 , Datateknikk, e det]";
-        ArrayList<Fag> result = instance.hentAlleFag();
-        String res = "";
-        for(Fag f: result) {
-            res += "[" +  f.getFagkode() + ", " + f.getNavn() + "]";
-        }
-        res += "]";
-        assertEquals(expResult, res);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testOppdaterBruker() throws Exception {
-        System.out.println("oppdaterBruker");
-        String brukernavn = "";
-        int brukertype = 0;
-        String navn = "";
-        String passord = "";
-        String mail = "";
-        
-        instance.oppdaterBruker(brukernavn, brukertype, navn, passord, mail);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of oppdaterRom method, of class DbConnection.
-     */
-    @Test
-    public void testOppdaterRom() throws Exception {
-        System.out.println("oppdaterRom");
-        String romnr = "";
-        int etasje = 0;
-        int plasser = 0;
-        boolean harSmartboard = false;
-        boolean harSkjerm = false;
-        boolean harProsjektor = false;
-        int tilgang = 0;
-        
-        instance.oppdaterRom(romnr, etasje, plasser, harSmartboard, harSkjerm, harProsjektor, tilgang);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -204,20 +106,6 @@ public class DbConnectionTest {
         instance.oppdaterPassord(brukernavn, passord);
         assertEquals(instance.hentPassord("sevaD"), instance.sha1("passord2"));
         instance.oppdaterPassord(brukernavn, "passord");
-    }
-
-    /**
-     * Test of oppdaterMail method, of class DbConnection.
-     */
-    @Test
-    public void testOppdaterMail() throws Exception {
-        System.out.println("oppdaterMail");
-        String brukernavn = "";
-        String mail = "";
-        
-        instance.oppdaterMail(brukernavn, mail);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -244,32 +132,6 @@ public class DbConnectionTest {
         String expResult = "173, 0, SjurRin, 102, 30-01-2016-06-00, 30-01-2016-07-00, true";
         Booking result = instance.getBooking(bookingId);
         assertEquals(expResult, result.getBookingId() + ", " + result.getBrukertype() + ", " + result.getBrukernavn() + ", "+ result.getRomNummer()+ ", " + result.getFratid() + ", " + result.getTiltid() + ", " + result.getSjekketInn());
-    }
-
-    /**
-     * Test of setSjekketInn method, of class DbConnection.
-     */
-    @Test
-    public void testSetSjekketInn() throws Exception {
-        System.out.println("setSjekketInn");
-        int bookingId = 0;
-        
-        instance.setSjekketInn(bookingId);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of fjernBooking method, of class DbConnection.
-     */
-    @Test
-    public void testFjernBooking() throws Exception {
-        System.out.println("fjernBooking");
-        int bookingId = 0;
-        
-        instance.fjernBooking(bookingId);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -365,71 +227,6 @@ public class DbConnectionTest {
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of hentBooking method, of class DbConnection.
-     */
-    @Test
-    public void testHentBooking_String() throws Exception {
-        System.out.println("hentBooking");
-        String romnr = "102";
-        
-        ArrayList<Booking> expResult = new ArrayList();
-        expResult.add(new Booking("SjurRin", "102", "30-01-2016-06-00", "30-01-2016-07-00", 0));
-        expResult.get(0).setBookingId(173);
-        expResult.add(new Booking("SjurRin", "102", "25-02-2016-14-00", "25-02-2016-15-00", 1));
-        expResult.get(1).setBookingId(303);
-        ArrayList<Booking> result = instance.hentBooking(romnr);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of regBooking method, of class DbConnection.
-     */
-    @Test
-    public void testRegBooking() throws Exception {
-        System.out.println("regBooking");
-        String brukernavn = "";
-        Booking b = null;
-        int brukerType = 0;
-        ArrayList<Booking> personBook = null;
-        
-        boolean expResult = false;
-        boolean result = instance.regBooking(brukernavn, b, brukerType, personBook);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of generateAndSendEmail method, of class DbConnection.
-     */
-    @Test
-    public void testGenerateAndSendEmail() throws Exception {
-        System.out.println("generateAndSendEmail");
-        String brukernavn = "";
-        String oppdatering = "";
-        String header = "";
-        
-        instance.generateAndSendEmail(brukernavn, oppdatering, header);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of hentAlleStudier method, of class DbConnection.
-     */
-    @Test
-    public void testHentAlleStudier() throws Exception {
-        System.out.println("hentAlleStudier");
-        
-        ArrayList<Studium> expResult = null;
-        ArrayList<Studium> result = instance.hentAlleStudier();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of hentAlleStudenter method, of class DbConnection.
@@ -443,49 +240,4 @@ public class DbConnectionTest {
         String[] result = instance.hentAlleStudenter(brukertype);
         assertArrayEquals(expResult, result);
     }
-
-    /**
-     * Test of hentStudenterIStudium method, of class DbConnection.
-     */
-    @Test
-    public void testHentStudenterIStudium() throws Exception {
-        System.out.println("hentStudenterIStudium");
-        String studienavn = "";
-        
-        String[] expResult = null;
-        String[] result = instance.hentStudenterIStudium(studienavn);
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of leggTilStudIStudium method, of class DbConnection.
-     */
-    @Test
-    public void testLeggTilStudIStudium() throws Exception {
-        System.out.println("leggTilStudIStudium");
-        String brukernavn = "";
-        String studiekode = "";
-        String studienavn = "";
-        
-        instance.leggTilStudIStudium(brukernavn, studiekode, studienavn);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of slettStudIStudium method, of class DbConnection.
-     */
-    @Test
-    public void testSlettStudIStudium() throws Exception {
-        System.out.println("slettStudIStudium");
-        String brukernavn = "";
-        String studiekode = "";
-        
-        instance.slettStudIStudium(brukernavn, studiekode);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
